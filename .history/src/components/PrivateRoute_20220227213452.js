0e3ae2c18ed2e037a70e3b241449ffccfbf35040
lomelo_x <91914856+lomelo-x@users.noreply.github.com>
2022-02-27 21:36:41 +0100

    home component WIP

---
+++ .history/src/components/PrivateRoute_20220227213452.js
@@ -0,0 +1,6 @@
+import { Navigate } from 'react-router-dom';
+
+export default function PrivateRoute({ children, redirectTo }) {
+	const token = localStorage.getItem('token');
+	return token ? children : <Navigate to={redirectTo} />;
+}
